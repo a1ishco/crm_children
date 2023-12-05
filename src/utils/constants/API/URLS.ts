7df06ed734883674e4acab72905344607ccb02cd
Ali Bashirov <ali.bashirov02@gmail.com>
2023-12-05 13:50:12 +0400

    Env created and bugs fixed

--- src/utils/constants/API/URLS.ts
+++ src/utils/constants/API/URLS.ts
@@ -1,8 +1,8 @@
-export const API_LOGIN = "https://crm-api.qmeter.net/api/login/";
-export const API_ME = "https://crm-api.qmeter.net/api/me/";
-export const API_LOGOUT = "https://crm-api.qmeter.net/api/logout/";
-export const API_CHILDREN_GET = "https://w4st1b2x-8754.euw.devtunnels.ms/api/customers/";
-export const API_CHILDREN_CREATE = "https://w4st1b2x-8754.euw.devtunnels.ms/api/customers/create/";
-export const API_CHILDREN_UPDATE = "https://w4st1b2x-8754.euw.devtunnels.ms/api/customers/update/";
-export const API_CHILDREN_DELETE = "https://w4st1b2x-8754.euw.devtunnels.ms/api/customers/delete/";
-export const API_CHILDREN_PAYMENT = "https://w4st1b2x-8754.euw.devtunnels.ms/api/customers/";
+export const API_LOGIN = `${import.meta.env.VITE_API_BASE_URL}login/`;
+export const API_ME = `${import.meta.env.VITE_API_BASE_URL}me/`;
+export const API_LOGOUT = `${import.meta.env.VITE_API_BASE_URL}logout/`;
+export const API_CHILDREN_GET = import.meta.env.VITE_API_CHILDREN_BASE_URL;
+export const API_CHILDREN_CREATE = `${import.meta.env.VITE_API_CHILDREN_BASE_URL}create/`;
+export const API_CHILDREN_UPDATE = `${import.meta.env.VITE_API_CHILDREN_BASE_URL}update/`;
+export const API_CHILDREN_DELETE = `${import.meta.env.VITE_API_CHILDREN_BASE_URL}delete/`;
+export const API_CHILDREN_PAYMENT = import.meta.env.VITE_API_CHILDREN_BASE_URL;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    VITE_API_BASE_URL: process.env.VITE_API_BASE_URL || 'https://crm-api.qmeter.net/api/',
+    VITE_API_CHILDREN_BASE_URL: process.env.VITE_API_CHILDREN_BASE_URL || 'https://w4st1b2x-8754.euw.devtunnels.ms/api/customers/',
+  },
+});
